e8f91dd7d7d385ee35331249bd9f44715d7f2ccf
Shivansh Sharma <raxisbaised@gmail.com>
2026-03-28 19:17:53 +0530

    Create traversall_in_array.cpp

---
+++ traversall_in_array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[10] = {8,6,77,14,5};
+  int n = 5;
+
+  for(int i =0 ; i<n ; i ++){
+     cout << arr[i] << " ";
+    }
+    cout << endl;
