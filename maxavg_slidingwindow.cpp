fc0ed01d13eb79895fd91e6ae74da3ce7e60e754
Shivansh Sharma <raxisbaised@gmail.com>
2026-03-29 21:46:04 +0530

    Add maxSubarraySum function for sliding window average
    Implement maxSubarraySum function to calculate maximum average of subarrays of size k.

---
+++ maxavg_slidingwindow.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubarraySum(vector<int>&arr,int k ) {
+  int n = arr.size();
+  if(n<k) return -1;
+  int windowsum = 0;
+  for(int i = 0 ; i<k ; i++){
+      windowsum += arr[i];
+  }
+  int maxsum = windowsum;
+  for(int i = k ; i<n ; i++) {
+      (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];  // add new element, remove old
+        maxSum = max(maxSum, windowSum);
+    }
+
+    return (double)maxSum/k;
+}
