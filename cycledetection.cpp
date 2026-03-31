5449cc92adfb929b7dac1aa15e8a3ea09d0a31fb
Shivansh Sharma <raxisbaised@gmail.com>
2026-03-31 22:42:01 +0530

    Implement cycle detection in linked list

---
+++ cycledetection.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode* head) {
+    if (!head || !head->next) return false;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          // move 1 step
+        fast = fast->next->next;    // move 2 steps
+
+        if (slow == fast) {
+            return true;            // cycle detected
+        }
+    }
+    return false;                   // no cycle
+}
