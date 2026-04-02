2d94ff074f9c48f6b4970be019ab87986e2d4f56
Shivansh Sharma <raxisbaised@gmail.com>
2026-04-02 20:10:33 +0530

    Implement totalFruits function for basket problem

---
+++ fruits_into_basket.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int totalFruits(vector<int>& fruits) {
+  unordered_map<int, int> basket;
+  int left = 0 , maxLen = 0;
+
+  for (int right = 0; right < fruits.size() ; right ++ ) {
+      basket[fruits[right]]++;
+
+      while ( basket.size() > 2 ) {
+        basket[fruits[left]]--;
+        if (basket[fruit[left]] == 0 ) {
+              basket.erase(fruits[left]);
+        }
+      left++;
+      }
+maxLen = max(maxLen, right - left + 1);
+  }
+return maxLen;
+}
